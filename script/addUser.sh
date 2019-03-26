c3f0e7b3193833c8e3d27dc09b8cabe86b532600
Kevin Jonathan Harnanta <crossmajor99@gmail.com>
2019-03-26 07:00:40 +0700

    Add files via upload

--- script/addUser.sh
+++ script/addUser.sh
@@ -4,4 +4,41 @@
 useradd -m -s /bin/bash $1
 echo -e "$2\n$2\n" | passwd $1
 echo "$1" >> /etc/vsftpd.userlist
-chmod 0700 /home/$1
+chmod 0755 /home/$1
+
+cat << EOF > /opt/nginxconf/$1.conf
+
+server {
+    listen       80;
+    server_name  $1.ku;
+    root   /usr/share/nginx/html/$1/;
+    index index.php  index.html index.htm;
+    
+
+    error_page   500 502 503 504  /50x.html;
+    location = /50x.html {
+        root   /usr/share/nginx/html/;
+    }
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+   
+    error_page 404 /404.html;
+    location = /404.html {
+	root /usr/share/nginx/html;
+    }
+
+    location ~ [^/]\.php(/|$) {
+    fastcgi_split_path_info  ^(.+\.php)(/.+)$;
+    fastcgi_index            index.php;
+    fastcgi_pass             unix:/var/run/php-fpm.sock;
+    include                  fastcgi_params;
+    fastcgi_param   PATH_INFO       $fastcgi_path_info;
+    fastcgi_param   SCRIPT_FILENAME /usr/share/nginx/html/$1\$fastcgi_script_name;
+   }
+}
+
+
+EOF
+
+/opt/script/addrecord.sh $1.ku 192.168.220.128
