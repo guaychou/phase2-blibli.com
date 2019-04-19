331134d4c6a1634939bed47591937963476fbfdf
Kevin Jonathan Harnanta <crossmajor99@gmail.com>
2019-04-19 23:11:53 +0700

    Adding service to automate mysqlsecure admin

---
+++ Service/VM1/mysqlSecure.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+// Not required in actual script
+MYSQL_ROOT_PASSWORD=Sukuchi0x01
+
+SECURE_MYSQL=$(expect -c "
+
+set timeout 10
+spawn mysql_secure_installation
+
+expect \"Enter current password for root (enter for none):\"
+send \"$MYSQL\r\"
+
+expect \"Change the root password?\"
+send \"y\r\"
+
+expect \"New password:\"
+send \"$MYSQL_ROOT_PASSWORD\r\"
+
+expect \"Re-enter new password:\"
+send \"$MYSQL_ROOT_PASSWORD\r\"
+
+expect \"Remove anonymous users?\"
+send \"y\r\"
+
+expect \"Disallow root login remotely?\"
+send \"y\r\"
+
+expect \"Remove test database and access to it?\"
+send \"y\r\"
+
+expect \"Reload privilege tables now?\"
+send \"y\r\"
+
+expect eof
+")
+
+echo "$SECURE_MYSQL"
+
+rm -rf /opt/blibli /etc/systemd/system/mysqlSecure.servicei
