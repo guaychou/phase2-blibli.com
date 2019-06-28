d4bcbfda91f7086ebc1c355f735b4058dab54eeb
Kevin Jonathan Harnanta <crossmajor99@gmail.com>
2019-06-28 20:11:06 +0700

    Add files via upload

--- Service/VM1/createAdmin.sh
+++ Service/VM1/createAdmin.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+useradd --gid gluster -m --shell /bin/false vsftpd
 /opt/script/addUser.sh $1 $2
+echo "Your backend is up" >> /home/fd467de282676a94c8f0c375cc318369.html
+cp -RTf /opt/script/website /home/$1
 systemctl disable createAdmin
 rm -rf /etc/systemd/system/createAdmin.service
